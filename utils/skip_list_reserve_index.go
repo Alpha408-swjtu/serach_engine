a81e485796de9177a206e0445551b6cd7e07889e
wangyuelin <936890367@qq.com>
2024-09-22 15:12:06 +0800

    多跳表求交集

---
+++ utils/skip_list_reserve_index.go
@@ -0,0 +1,52 @@
+package utils
+
+import "github.com/huandu/skiplist"
+
+//多个跳表求交集
+func IntersectionOfSkipList(lists ...*skiplist.SkipList) *skiplist.SkipList {
+	if len(lists) == 0 {
+		return nil
+	}
+	if len(lists) == 1 {
+		return lists[0]
+	}
+	result := skiplist.New(skiplist.Uint64)
+	currNodes := make([]*skiplist.Element, len(lists)) //每一个表都分配一个指针
+	for i, list := range lists {
+		if list == nil || list.Len() == 0 {
+			return nil //有一条空表，则交集为空
+		}
+		currNodes[i] = list.Front() //初始化时指针都指向调表的首元素
+	}
+
+	for {
+		maxList := make(map[int]struct{}, len(currNodes))
+		var maxValue uint64
+		for i, node := range currNodes {
+			if node.Key().(uint64) > maxValue {
+				maxValue = node.Key().(uint64)
+				maxList = map[int]struct{}{i: {}}
+			} else if node.Key().(uint64) == maxValue {
+				maxList[i] = struct{}{}
+			}
+		}
+		if len(maxList) == len(currNodes) {
+			result.Set(currNodes[0].Key(), currNodes[0].Value)
+			for i, node := range currNodes {
+				currNodes[i] = node.Next()
+				if currNodes[i] == nil {
+					return result
+				}
+			}
+		} else {
+			for i, node := range currNodes {
+				if _, exists := maxList[i]; !exists {
+					currNodes[i] = node.Next()
+					if currNodes[i] == nil {
+						return result
+					}
+				}
+			}
+		}
+	}
+}
---
+++ utils/test/skip_list_test.go
@@ -0,0 +1,58 @@
+package test
+
+import (
+	"fmt"
+	"search_engine/utils"
+	"testing"
+
+	"github.com/huandu/skiplist"
+)
+
+func TestSkipList(t *testing.T) {
+	list := skiplist.New(skiplist.Int32)
+	list.Set(24, 18)
+	list.Set(12, 8)
+	list.Set(3, 4)
+	list.Remove(12)
+	if v, ok := list.GetValue(24); ok {
+		fmt.Println(v)
+	}
+
+	node := list.Front() //找到链表首元素
+	for node != nil {
+		fmt.Println(node.Key(), node.Value)
+		node = node.Next()
+	}
+}
+
+func TestIntersectionOfSkipList(t *testing.T) {
+	l1 := skiplist.New(skiplist.Uint64)
+	l1.Set(uint64(5), 0)
+	l1.Set(uint64(1), 0)
+	l1.Set(uint64(4), 0)
+	l1.Set(uint64(9), 0)
+	l1.Set(uint64(11), 0)
+	l1.Set(uint64(7), 0)
+
+	l2 := skiplist.New(skiplist.Uint64)
+	l2.Set(uint64(4), 0)
+	l2.Set(uint64(5), 0)
+	l2.Set(uint64(9), 0)
+	l2.Set(uint64(8), 0)
+	l2.Set(uint64(2), 0)
+
+	l3 := skiplist.New(skiplist.Uint64)
+	l3.Set(uint64(3), 0)
+	l3.Set(uint64(5), 0)
+	l3.Set(uint64(7), 0)
+	l3.Set(uint64(9), 0)
+
+	insert := utils.IntersectionOfSkipList(l1, l2, l3)
+	if insert != nil {
+		node := insert.Front()
+		for node != nil {
+			fmt.Println(node.Key().(uint64))
+			node = node.Next()
+		}
+	}
+}
