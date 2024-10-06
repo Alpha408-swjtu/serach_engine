132365e28021564b64a2e32a2ca92af53734c727
wangyuelin <936890367@qq.com>
2024-10-06 20:24:20 +0800

    etcd注册注销查询功能

--- index_service/service_hub.go
+++ index_service/service_hub.go
@@ -1,12 +1,16 @@
 package index_service
 
 import (
+	"context"
 	"log"
+	"strings"
 	"sync"
 	"time"
 
 	etcdv3 "go.etcd.io/etcd/client/v3"
 )
+
+const SERVICE_ROOT_PATH = "/alpha/index"
 
 // 用etcd实现服务注册与发现
 type ServiceHub struct {
@@ -38,4 +42,59 @@ func GetServiceHub(etcdServers []string, heartbeayFrequency int) *ServiceHub {
 		})
 	}
 	return serviceHub
+}
+
+// 注册功能
+func (hub *ServiceHub) Regist(service string, endpoint string, leaseId *etcdv3.LeaseID) (etcdv3.LeaseID, error) {
+	ctx := context.Background()
+	if int(*leaseId) <= 0 {
+		if lease, err := hub.Client.Grant(ctx, int64(hub.heartbeatFrequency)); err != nil {
+			log.Printf("创建租约失败:%v", err)
+			return 0, err
+		} else {
+			key := strings.TrimRight(SERVICE_ROOT_PATH, "/") + "/" + service + "/" + endpoint
+			if _, err := hub.Client.Put(ctx, key, "", etcdv3.WithLease(lease.ID)); err != nil {
+				log.Printf("服务注册失败:%s", err)
+				return lease.ID, err
+			} else {
+				return lease.ID, nil
+			}
+		}
+	} else {
+		if _, err := hub.Client.KeepAliveOnce(ctx, *leaseId); err != nil {
+			return 0, err
+		} else {
+			return *leaseId, nil
+		}
+	}
+}
+
+// 注销
+func (hub *ServiceHub) UnRegist(service string, endPoint string) error {
+	ctx := context.Background()
+	key := strings.TrimRight(SERVICE_ROOT_PATH, "/") + "/" + service + "/" + endPoint
+	if _, err := hub.Client.Delete(ctx, key); err != nil {
+		log.Printf("注销服务:%s对应节点:%s失败:%v", service, endPoint, err)
+		return err
+	} else {
+		log.Printf("注销成功")
+		return nil
+	}
+}
+
+func (hub *ServiceHub) GetServiceEndpoint(service string) []string {
+	ctx := context.Background()
+	prefix := strings.TrimRight(SERVICE_ROOT_PATH, "/") + "/" + service + "/"
+	if rep, err := hub.Client.Get(ctx, prefix, etcdv3.WithPrefix()); err != nil {
+		log.Printf("获取节点失败")
+		return nil
+	} else {
+		endpoints := make([]string, 0, len(rep.Kvs))
+		for _, kv := range rep.Kvs {
+			path := strings.Split(string(kv.Key), "/")
+			endpoints = append(endpoints, path[len(path)-1])
+		}
+		log.Printf("获取节点成功")
+		return endpoints
+	}
 }
